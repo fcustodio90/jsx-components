58942848ff785fb29504a3e2937d51ee9a3db6ea
filipe custodio <filipe.l.custodio@gmail.com>
2019-06-27 02:14:00 +0100

    Introduction to React Parent-children and component reusability

--- src/index.js
+++ src/index.js
@@ -8,10 +8,24 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail avatar= {faker.image.avatar()} author="Sam" timeAgo="Today at 4:45PM" content="This is a nice post!"/>
-      <CommentDetail avatar= {faker.image.avatar()} author="Alex" timeAgo="Today at 2:45PM" content="This is a nicer post!" />
-      <CommentDetail avatar= {faker.image.avatar()} author="Jane" timeAgo="Yesterday at 5:00AM" content="This is the nicest post!"/>
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail avatar= {faker.image.avatar()} author="Sam" timeAgo="Today at 4:45PM" content="This is a nice post!"/>
+      </ApprovalCard>
+      
+      <ApprovalCard>
+        <CommentDetail avatar= {faker.image.avatar()} author="Alex" timeAgo="Today at 2:45PM" content="This is a nicer post!" />
+      </ApprovalCard>
+    
+      <ApprovalCard>
+        <CommentDetail avatar= {faker.image.avatar()} author="Jane" timeAgo="Yesterday at 5:00AM" content="This is the nicest post!"/>
+      </ApprovalCard>
     </div>
   );
 };
